f394e11601e0d682bed0c0984400423c3b35a3c3
Javier Gamarra <nhpatt@gmail.com>
2013-11-16 01:38:57 +0100

    090-fin

--- test/com/nhpatt/ptd/doubles/stories/spy/ProyectAndSpiesTests.java
+++ test/com/nhpatt/ptd/doubles/stories/spy/ProyectAndSpiesTests.java
@@ -1,18 +1,20 @@
-package com.nhpatt.ptd.doubles.stories.mock;
+package com.nhpatt.ptd.doubles.stories.spy;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import com.nhpatt.ptd.doubles.stories.User;
 import com.nhpatt.ptd.doubles.stories.stub.Database;
 import com.nhpatt.ptd.doubles.stories.stub.Proyect;
 
-public class ProyectAndMocksTests {
+public class ProyectAndSpiesTests {
 
 	private Proyect proyect;
 
@@ -21,6 +23,9 @@ public class ProyectAndMocksTests {
 
 	@Mock
 	private User juan;
+	
+	@Spy
+	private User pepe;
 
 	@Before
 	public void setUp() {
@@ -35,5 +40,16 @@ public class ProyectAndMocksTests {
 		proyect.close(database);
 
 		verify(juan).closePendingTasks(proyect);
+		Assert.assertEquals(0, juan.getFailedProyects());
+	}
+
+	@Test
+	public void youCanCloseAProyectAndSpyItTest() {
+		when(database.getUsers(proyect)).thenReturn(new User[] { pepe });
+
+		proyect.close(database);
+
+		verify(pepe).closePendingTasks(proyect);
+		Assert.assertEquals(1, pepe.getFailedProyects());
 	}
 }
